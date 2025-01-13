8f84e034ad07203de59df6189f8adc8ad88be091
ThyGram <Williamgram@outlook.dk>
2025-01-13 09:22:38 +0100

    Added Program to Github

---
+++ ComputerPartShop/ComputerPartShop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ComputerPartShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
