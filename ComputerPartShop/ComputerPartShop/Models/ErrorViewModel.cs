146d0686f0da1f01e6d97c04db5339e0ac1eb129
ThyGram <Williamgram@outlook.dk>
2025-03-10 09:23:53 +0100

    removing old files

--- ComputerPartShop/ComputerPartShop/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ComputerPartShop.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
